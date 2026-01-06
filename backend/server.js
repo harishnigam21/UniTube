333135a1f437fcf39469a61a353518338606c625
Harish Nigam <harishnigam21@gmail.com>
2026-01-06 08:00:15 +0530

    Add MongoDB connection with Mongoose to backend
    Introduced a DBConnection.js module to handle MongoDB connections using Mongoose. Updated server.js to connect to the database on startup. Added mongoose and jsonwebtoken dependencies, and updated .gitignore to exclude .env files.

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import { configDotenv } from "dotenv";
+import connectDB from "./DB/DBConnection.js";
 const app = express();
 configDotenv();
+connectDB();
 const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) =>
   res.status(200).json({ message: "UniTube Server is Running Perfect" })
