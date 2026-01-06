74d644c89f67c505249b2ad5042284335577571f
Harish Nigam <harishnigam21@gmail.com>
2026-01-06 18:11:05 +0530

    Add ID validation middleware to channel routes
    Introduced mongoose ID validation middleware for delete, update, and new subscriber channel routes to ensure valid IDs are provided in requests.

--- backend/routes/Channel.js
+++ backend/routes/Channel.js
@@ -6,9 +6,12 @@ import {
   updateChannel,
 } from "../controllers/Channel";
 import jwtVerifier from "../middlewares/jwt/jwtVerifier";
+import Validate from "../middlewares/validators/mongooseIDValidation";
 const router = express.Router();
 router.route("/create_channel").post(jwtVerifier, createChannel);
-router.route("/delete_channel").delete(jwtVerifier, deleteChannel);
-router.route("/update_channel").patch(jwtVerifier, updateChannel);
-router.route("/new_subscriber").patch(jwtVerifier, newSubscriber);
+router
+  .route("/delete_channel/:id")
+  .delete(Validate, jwtVerifier, deleteChannel);
+router.route("/update_channel/:id").patch(Validate, jwtVerifier, updateChannel);
+router.route("/new_subscriber/:id").patch(Validate, jwtVerifier, newSubscriber);
 export default router;
