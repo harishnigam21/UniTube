3e3ff48e933661a5fd288bbcfefcff2227667aef
Harish Nigam <harishnigam21@gmail.com>
2026-01-11 16:13:26 +0530

    Update Comment.js

--- backend/routes/Comment.js
+++ backend/routes/Comment.js
@@ -1,12 +1,18 @@
 import express from "express";
-import { deleteComment, getComment, postComment } from "../controllers/Comment.js";
+import {
+  deleteComment,
+  getComment,
+  postComment,
+  updateComment,
+} from "../controllers/Comment.js";
 import jwtVerifier from "../middlewares/jwt/jwtVerifier.js";
 import commentValidation from "../middlewares/validators/commentValidation.js";
 import Validate from "../middlewares/validators/mongooseIDValidation.js";
 const router = express.Router();
 router
   .route("/comment/:id")
-  .get(Validate, getComment)
+  .get(Validate, jwtVerifier, getComment)
   .post(Validate, commentValidation, jwtVerifier, postComment)
+  .patch(Validate, jwtVerifier, updateComment)
   .delete(Validate, jwtVerifier, deleteComment);
 export default router;
