9e99bfdea5b3b55d5f4d952992e13b7edffc892b
Harish Nigam <harishnigam21@gmail.com>
2026-01-11 13:45:26 +0530

    Update Comment.jsx

--- frontend/src/component/common/Comment.jsx
+++ frontend/src/component/common/Comment.jsx
@@ -3,21 +3,126 @@ import { getDaysBetween } from "../../utils/getDate";
 import { SlLike, SlDislike } from "react-icons/sl";
 import { FaChevronDown } from "react-icons/fa";
 import PostComment from "./PostComment";
+import { useSelector, useDispatch } from "react-redux";
+import { HiOutlineDotsVertical } from "react-icons/hi";
+import { MdOutlineDeleteOutline, MdOutlineModeEdit } from "react-icons/md";
+import { Link } from "react-router-dom";
+import {
+  deleteItemComment,
+  updateItemComment,
+} from "../../store/Slices/videoSlice";
 export default function Comment({ comm, postid }) {
+  const dispatch = useDispatch();
   const [toggleReply, setToggleReply] = useState(false);
   const [toggleReplies, setToggleReplies] = useState(false);
+  const [commentOption, seCommentOption] = useState(false);
+  const [updateCommentTxt, setUpdateCommentTxt] = useState("");
+  const user = useSelector((store) => store.user.userInfo);
+  const handleEdit = async () => {
+    const url = `${import.meta.env.VITE_BACKEND_HOST}/comment/${comm._id}`;
+    const token = window.localStorage.getItem("acTk");
+    if (updateCommentTxt.length > 1) {
+      const response = await fetch(url, {
+        method: "PATCH",
+        headers: {
+          "content-type": "application/json",
+          authorization: `bearer ${token ? JSON.parse(token) : ""}`,
+        },
+        body: JSON.stringify({ txt: updateCommentTxt }),
+        credentials: "include",
+      });
+      const responseData = await response.json();
+      console.log(responseData.message);
+      if (response.ok) {
+        dispatch(
+          updateItemComment({
+            commentId: comm._id,
+            updatedText: updateCommentTxt,
+          })
+        );
+        setUpdateCommentTxt("");
+      }
+    }
+  };
+  const handleDelete = async () => {
+    const url = `${import.meta.env.VITE_BACKEND_HOST}/comment/${comm._id}`;
+    const token = window.localStorage.getItem("acTk");
+    const response = await fetch(url, {
+      method: "DELETE",
+      headers: {
+        "content-type": "application/json",
+        authorization: `bearer ${token ? JSON.parse(token) : ""}`,
+      },
+      credentials: "include",
+    });
+    const responseData = await response.json();
+    console.log(responseData.message);
+    if (response.ok) {
+      dispatch(deleteItemComment({ commentIdToDelete: comm._id }));
+    }
+  };
+  const handleReport = async () => {}; //TODO:Complete it later, currently not necessary
   return (
     <article className="flex gap-4 w-full">
-      <img
-        src={comm.avatar}
-        alt=""
-        className="w-8 h-8 object-center object-cover aspect-square rounded-full"
-      />
+      <div className="flex w-6 h-6 aspect-square items-center justify-center rounded-full border p-1 text-fit text-[10px] border-txlight">
+        <span>{comm.user_id.firstname[0].toUpperCase()}</span>
+        <span>{comm.user_id.lastname[0].toUpperCase()}</span>
+      </div>
       <div className="flex flex-col gap-2 w-full">
-        <small className="flex gap-2">
-          <span className="font-medium">{comm.user_id.firstname}</span>
-          <span className="text-txlight">{getDaysBetween(comm.postedAt)}</span>
-        </small>
+        <div className="flex gap-4 justify-between w-full items-center">
+          <div className="flex gap-2 items-center">
+            <span className="font-medium">
+              {comm.user_id._id == user._id
+                ? "you"
+                : comm.user_id.email.split("@")[0]}
+            </span>
+            <span className="text-txlight">
+              {getDaysBetween(comm.postedAt)}
+            </span>
+          </div>
+          <div className="relative flex flex-col">
+            <div
+              className="flex items-center icon"
+              onClick={() => seCommentOption((prev) => !prev)}
+            >
+              <HiOutlineDotsVertical className="text-xl" />
+            </div>
+            {commentOption && (
+              <div
+                className="flex flex-col justify-center absolute min-w-fit py-2 px-6 bg-border mt-6 right-0 rounded-md overflow-hidden"
+                onMouseLeave={() => seCommentOption(false)}
+              >
+                {comm.user_id._id == user._id ? (
+                  <>
+                    <Link
+                      className="pb-2 flex flex-nowrap items-center gap-2"
+                      onClick={handleEdit}
+                    >
+                      <div className="icon flex items-center">
+                        <MdOutlineModeEdit className="text-xl" />
+                      </div>
+                      <span>Edit</span>
+                    </Link>
+                    <p className="border-txlight/10 border-b w-full scale-x-200"></p>
+                    <Link
+                      className="pt-2 flex flex-nowrap items-center gap-2"
+                      onClick={handleDelete}
+                    >
+                      <div className="icon flex items-center">
+                        <MdOutlineDeleteOutline className="text-xl" />
+                      </div>
+                      <span>Delete</span>
+                    </Link>
+                  </>
+                ) : (
+                  <Link className="py-1" onClick={handleReport}>
+                    Report
+                  </Link>
+                )}
+              </div>
+            )}
+          </div>
+        </div>
         <p className="text-[16px]">{comm.commentText}</p>
         <div className="flex flex-wrap items-center gap-4 text-sm">
           <div className="icon flex gap-2 items-center">
@@ -57,7 +162,9 @@ export default function Comment({ comm, postid }) {
               <p>{comm.replies.length} replies </p>
               <div className="mt-1">
                 <FaChevronDown
-                  className={`${toggleReplies ? "-rotate-90" : "rotate-0"} transition-all`}
+                  className={`${
+                    toggleReplies ? "-rotate-90" : "rotate-0"
+                  } transition-all`}
                 />
               </div>
             </div>
