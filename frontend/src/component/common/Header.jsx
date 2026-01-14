5596a5e2367f24600971468853ca11c5c222e105
Harish Nigam <harishnigam21@gmail.com>
2026-01-14 15:18:32 +0530

    Update Header.jsx

--- frontend/src/component/common/Header.jsx
+++ frontend/src/component/common/Header.jsx
@@ -1,10 +1,28 @@
 import { useSelector } from "react-redux";
 import items from "../../assets/data/static/header";
 import { Link } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
 export default function Header({ navToggle, setNavToggle, setSidebarToggle }) {
+  const headerRef = useRef(null);
+  const [headerHeight, setHeaderHeight] = useState(0);
+  const [expandProfile, setExpandProfile] = useState(false);
   const login = useSelector((store) => store.user.loginStatus);
+  const handleHeight = () => {
+    if (headerRef.current) {
+      let need = headerRef.current.getBoundingClientRect();
+      setHeaderHeight(need.height);
+    }
+  };
+  useEffect(() => {
+    handleHeight();
+    window.addEventListener("resize", handleHeight);
+    return () => window.removeEventListener("resize", handleHeight);
+  }, []);
   return (
-    <header className="flex flex-col w-full justify-between sticky top-0 py-1.5 px-4 overflow-hidden z-50 bg-bgprimary">
+    <header
+      ref={headerRef}
+      className="flex flex-col w-full justify-between sticky top-0 py-1.5 px-4 z-50 bg-bgprimary"
+    >
       <article className="flex gap-8 justify-between items-center">
         <article className="flex gap-4 items-center">
           {items
@@ -61,7 +79,7 @@ export default function Header({ navToggle, setNavToggle, setSidebarToggle }) {
             )
           )}
         </article>
-        <article className=" flex gap-4 items-center text-text">
+        <article className="flex gap-4 items-center text-text">
           {login
             ? items.slice(-4, -1).map((item) =>
                 item.name.toLowerCase() == "create" ? (
@@ -73,17 +91,38 @@ export default function Header({ navToggle, setNavToggle, setSidebarToggle }) {
                     <item.icon className="text-2xl icon" />
                     <span className="whitespace-nowrap">{item.name}</span>
                   </Link>
-                ) : (
-                  <Link
-                    to={item.path}
+                ) : item.name == "profile" ? (
+                  <div
                     key={`header/item/${item.id}`}
                     className="flex items-center"
                   >
                     <item.icon
-                      className={`${
-                        item.name == "profile" ? "text-3xl" : "text-2xl"
-                      } icon`}
+                      className={`text-3xl icon`}
+                      onClick={() => {
+                        setExpandProfile((prev) => !prev);
+                      }}
                     />
+                    {expandProfile && (
+                      <article
+                        className="flex flex-col absolute right-0 top-0 gap-4 rounded-b-xl p-4 z-50 bg-bgprimary min-w-fit whitespace-nowrap border border-border border-t-0"
+                        style={{ marginTop: `${headerHeight}px` }}
+                        onMouseLeave={() => {
+                          setExpandProfile(false);
+                        }}
+                      >
+                        <strong>My Posts</strong>
+                        <strong>My Channel</strong>
+                        <strong>Log Out</strong>
+                      </article>
+                    )}
+                  </div>
+                ) : (
+                  <Link
+                    to={item.path}
+                    key={`header/item/${item.id}`}
+                    className="relative flex items-center"
+                  >
+                    <item.icon className={`text-2xl icon`} />
                   </Link>
                 )
               )
